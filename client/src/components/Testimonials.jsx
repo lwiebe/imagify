6ed47f0fb9591d2e45e7088761ad6fd176fb07aa
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2024-12-20 14:08:15 +0100

    framer motion

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { assets, testimonialsData } from '../assets/assets'
+import { motion } from 'framer-motion'
 
 const Testimonials = () => {
     return (
-        <div className='flex flex-col items-center justify-center my-20 py-12'>
+        <motion.div className='flex flex-col items-center justify-center my-20 py-12'
+            initial={{ opacity: 0.2, y: 100 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1, y: 1 }}
+            viewport={{ once: true }}
+        >
             <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Customer testimonials</h1>
             <p className='text-gray-500 mb-8'>What Our Users Are Saying</p>
 
             <div className='flex flex-wrap gap-6'>
                 {testimonialsData.map((testimonial, index) => (
-                    <div key={index} 
-                    className='bg-white/20 p-12 rounded-lg shadow-md border w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all duration-300'>
+                    <div key={index}
+                        className='bg-white/20 p-12 rounded-lg shadow-md border w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all duration-300'>
                         <div className='flex flex-col items-center'>
                             <img src={testimonial.image} alt=""
                                 className='rounded-full w-14' />
@@ -27,7 +33,7 @@ const Testimonials = () => {
                 ))}
             </div>
 
-        </div>
+        </motion.div>
     )
 }
 
