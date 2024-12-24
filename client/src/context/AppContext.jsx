e691469101077a647654c457fa8b52680810a1c1
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2024-12-24 12:15:41 +0100

    working generation

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext()
 
@@ -12,6 +13,8 @@ const AppContextProvider = (props) => {
     const [credit, setCredit] = useState(false)
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
+    const navigate = useNavigate()
+
     const loadCreditsData = async () => {
         try {
             const { data } = await axios.get(backendUrl + '/api/user/credits', { headers: { token } })
@@ -22,6 +25,26 @@ const AppContextProvider = (props) => {
             }
         } catch (error) {
             console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+    const generateImage = async (prompt) => {
+        try {
+            const { data } = await axios.post(backendUrl + '/api/image/generate-image', { prompt }, { headers: { token } })
+
+            if (data.success) {
+                loadCreditsData()
+                return data.resultImage
+            } else {
+                toast.error(data.message)
+                loadCreditsData()
+                if (data.creditBalance === 0) {
+                    navigate('/buy')
+                }
+            }
+
+        } catch (error) {
             toast.error(error.message)
         }
     }
@@ -39,7 +62,7 @@ const AppContextProvider = (props) => {
     }, [token])
 
     const value = {
-        user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credit, setCredit, loadCreditsData, logout
+        user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credit, setCredit, loadCreditsData, logout, generateImage
     }
 
     return (
--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { motion } from 'framer-motion'
+import { AppContext } from '../context/AppContext'
 
 const Result = () => {
 
@@ -9,15 +10,27 @@ const Result = () => {
   const [loading, setLoading] = useState(false)
   const [input, setInput] = useState('')
 
-  const onSubmitHandler = async (e) => {
+  const { generateImage } = useContext(AppContext)
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    if (input) {
+      const image = await generateImage(input)
+      if (image) {
+        setIsImageLoaded(true)
+        setImage(image)
+      }
+    }
+    setLoading(false)
   }
 
   return (
     <motion.form onSubmit={onSubmitHandler} className='flex flex-col min-h-[90vh] justify-center items-center'
       initial={{ opacity: 0.2, y: 100 }}
       transition={{ duration: 1 }}
-      whileInView={{ opacity: 1, y: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
     >
       <div>
